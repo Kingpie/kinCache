9b70393a888070fef49c84170b65bd17fd37cd3d
liujinpei <liujinpei02@inke.cn>
2020-06-09 20:11:38 +0800

    comment

--- consistent_hash/consistent_hash_test.go
+++ consistent_hash/consistent_hash_test.go
@@ -7,11 +7,11 @@ import (
 
 func TestMap_AddNode(t *testing.T) {
 	m := New(5, nil)
-	m.AddNode("hehe", "haha", "lala")
+	m.AddNode("1", "2", "3")
 
 	fmt.Printf("%+v", m.keys)
 
-	testList := []string{"xixi", "dada", "gaga", "mimi"}
+	testList := []string{"gege", "dada", "gaga", "mimi"}
 
 	for _, s := range testList {
 		t.Logf("%s local node:%s", s, m.Get(s))
